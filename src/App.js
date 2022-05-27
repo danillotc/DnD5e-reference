5181e2df18953e83bc5a39231c26acfc40d568b2
danillotc <danillotc@hotmail.com>
2022-05-26 22:46:34 -0300

    implements routes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 import styled from 'styled-components';
-import GlobalFonts from './Assets/Fonts/fonts'
-import './App.css';
+import GlobalFonts from './Assets/Fonts/fonts';
 import Header from './Components/Header';
 import Races from './Pages/Races';
+import './App.css';
 
-const PaddedBody = styled.div`
-  padding: 30px;  
-`;
+const Container = styled.div`
+  padding: 20px 40px;
+`
 
 function App() {
   return (
-    <>
+    <Router>
       <GlobalFonts />
-      <Header title='DnD 5e Reference'/>
-      <PaddedBody>
-        <Races/>
-      </PaddedBody>
-    </>
+      <Header title='DnD 5e'/>
+      <Container>
+        <Routes>
+          <Route path='/' />
+          <Route path='/races' element={<Races />} />
+        </Routes>
+      </Container>      
+    </Router>
   );
 }
 
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,24 +1,47 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 const HeaderBox = styled.div`
     height: 5em;
+    width: 100%;
     padding: 0 1em;
     display: flex;
-    justify-content: center;
+    justify-content: space-between;
     align-items: center;
     background-color: black;
-`
+    `
 
 const Title = styled.h1`
     color: whitesmoke;
     font-size: 2rem;
     font-family: 'DeRoos';
 `
+const MenuBox = styled.div`
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    background-color: black;
+    padding-right: 1rem;
+`
+
+const MenuItem = styled.div`
+    font-size: 1.5rem;
+    padding-right: 2rem;
+    text-decoration: none;
+    color: white;
+`
 
 function Header(props) {
     return <HeaderBox>
-            <Title>{props.title}</Title>
+            <Link to='/' style={{textDecoration: 'none'}}>
+                <Title>{props.title}</Title>
+            </Link>
+            <MenuBox>
+                <Link to='/races' style={{textDecoration: 'none'}}>
+                    <MenuItem>Races</MenuItem>
+                </Link>
+            </MenuBox>
         </HeaderBox>
 };
 
