755b8662dbbbeeb79fefd99fb3281d08af7d2c16
danillotc <danillotc@hotmail.com>
2022-06-06 22:57:20 -0300

    implements myChar form

--- src/Pages/MyChar.jsx
+++ src/Pages/MyChar.jsx
@@ -1,7 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import Title from '../Components/Title';
+import TextField from '@mui/material/TextField';
+
+const Grid = styled.form`
+    display: grid;
+    width: 50%;
+    grid-template-columns: 2fr 1fr;
+    gap: 1em;
+`
 
 function MyChar() {
-    return <h1>My Character</h1>
+    const [state, setState] = useState({
+        name: '',
+        age: ''
+    });
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setState(prevState => ({
+            ...prevState,
+            [name]: value
+        }));
+    };
+
+    return <>
+        <Title>Enter your character information</Title>
+        <Grid>
+            <TextField id="charName" 
+                label="Name" 
+                variant="outlined" 
+                type="text"  
+                value={state.name}
+                name="name"
+                onChange={handleChange} />
+            <TextField id="charAge" 
+                label="Age" 
+                variant="outlined" 
+                type="text" 
+                inputProps={{ inputMode: "numeric", pattern: "[0-9]*" }} 
+                value={state.age}
+                name="age"
+                onChange={handleChange} />
+        </Grid>
+    </>
 }
 
 export default MyChar;
